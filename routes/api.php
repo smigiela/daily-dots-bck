0959f776c4d6a17c9f938bc50cfd625c9b9003aa
Daniel Śmigiela <daniel.smigiela@outlook.com>
2024-10-24 18:32:49 +0200

    włąączenie tokenów api

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Route::apiResource('/tasks', App\Http\Controllers\Api\Diary\TasksController::class)->middleware('auth:sanctum');
-Route::apiResource('/tasks', App\Http\Controllers\Api\Diary\TasksController::class);
-
-
+Route::apiResource('/tasks', App\Http\Controllers\Api\Diary\TasksController::class)->middleware('auth:sanctum');
